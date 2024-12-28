aee1d953d6566b412dd3ff26f1575c114ef0a0cb
shubham <shubhamsha0071@gmail.com>
2024-12-28 16:46:10 +0530

    Fix: Adds post list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import PostsForm from './components/posts/PostsForm';
+import PostsList from './PostsList';
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <PostsForm setPosts={setPosts} posts={posts} />
+      <PostsList posts={posts} />
     </div>
   );
 }
